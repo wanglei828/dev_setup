7f2a9cf9f9d99baa7fe28b4d20e466e1d4a52256
Lei Wang <wanglei63@baidu.com>
2017-09-11 15:04:19 -0700

    Fix run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,37 +1,44 @@
 #! /bin/bash
 
-if [ -z "${DOCKER_REPO}" ]; then
-    DOCKER_REPO=apolloauto/internal
-fi
+function init() {
+    if [ -z "${DOCKER_REPO}" ]; then
+        DOCKER_REPO=apolloauto/internal
+    fi
 
-if [[ $# != 1 ]];then
-    echo "Usage: run_docker.sh image_name"
-    exit 1
-fi
+    if [[ $# != 1 ]];then
+        echo "Usage: run_docker.sh image_name"
+        exit 1
+    fi
 
-VERSION=$1
-IMG=${DOCKER_REPO}:$VERSION
-LOCAL_DIR=$HOME
+    VERSION=$1
+    IMG=${DOCKER_REPO}:$VERSION
+    LOCAL_DIR=$HOME
 
-docker ps -a --format "{{.Names}}" | grep 'tmp_container' 1>/dev/null
-if [ $? == 0 ]; then
-    docker stop tmp_container 1>/dev/null
-    docker rm -f tmp_container 1>/dev/null
-fi
+    docker ps -a --format "{{.Names}}" | grep 'tmp_container' 1>/dev/null
+    if [ $? == 0 ]; then
+        docker stop tmp_container 1>/dev/null
+        docker rm -f tmp_container 1>/dev/null
+    fi
 
-local display=""
-if [[ -z ${DISPLAY} ]];then
-    display=":0"
-else
-    display=${DISPLAY}
-fi
+    display=""
+    if [[ -z ${DISPLAY} ]];then
+        display=":0"
+    else
+        display=${DISPLAY}
+    fi
+}
 
-docker run -it \
-    -d \
-    --name tmp_container \
-    -e DISPLAY=$display \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $LOCAL_DIR:/mnt \
-    --net host \
-    -w /mnt \
-    $IMG
+function main() {
+    init $1
+    docker run -it \
+        -d \
+        --name tmp_container \
+        -e DISPLAY=$display \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v $LOCAL_DIR:/mnt \
+        --net host \
+        -w /mnt \
+        $IMG
+}
+
+main $@
